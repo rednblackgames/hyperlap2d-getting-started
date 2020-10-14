05373af36b8380fffb3dabc38e3054e7bbd2af1a
fgnm <fgnm.dev@gmail.com>
2020-10-14 11:31:16 +0200

    Add HyperLap2D Example project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # HyperLap2D libGDX - Gtting Started
 HyperLap2D Runtime libGDX Getting Started example
+
+# License
+
+All graphic assets copyright belong to [craftpix.net](https://craftpix.net/freebies/free-lava-fields-level-map-2d-backgrounds/).
+
+```
+Copyright (c) 2020 Francesco Marongiu.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
--- core/src/main/java/games/rednblack/hyperlap2d/example/HyperLap2DExample.java
+++ core/src/main/java/games/rednblack/hyperlap2d/example/HyperLap2DExample.java
@@ -1,7 +1,54 @@
 package games.rednblack.hyperlap2d.example;
 
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import games.rednblack.editor.renderer.SceneLoader;
+import games.rednblack.editor.renderer.systems.action.Actions;
+import games.rednblack.editor.renderer.systems.action.data.ActionData;
+import games.rednblack.editor.renderer.utils.ItemWrapper;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class HyperLap2DExample extends ApplicationAdapter {
+
+    private SceneLoader mSceneLoader;
+    private Viewport mViewport;
+    private Camera mCamera;
+
+    @Override
+    public void create() {
+        mCamera = new OrthographicCamera();
+        mViewport = new ExtendViewport(360, 640, 0, 0, mCamera);
+        mSceneLoader = new SceneLoader();
+        mSceneLoader.loadScene("MainScene", mViewport);
+
+        //Add action from library
+        ItemWrapper root = new ItemWrapper(mSceneLoader.getRoot());
+
+        Entity skull3 = root.getChild("level-3-skull").getEntity();
+        ActionData actionData = mSceneLoader.loadActionFromLibrary("test");
+        Actions.addAction(mSceneLoader.getEngine(),  skull3, actionData);
+    }
+
+    @Override
+    public void render() {
+        mCamera.update();
+
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        mViewport.apply();
+        mSceneLoader.getEngine().update(Gdx.graphics.getDeltaTime());
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        mViewport.update(width, height);
+    }
 }
